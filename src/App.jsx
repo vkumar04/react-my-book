d59b5796efc74aecb84b917d7739bc462501b647
Vick Kumar <vick.kumar44@gmail.com>
2017-07-25 19:52:21 -0400

    get book data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import * as BooksAPI from './BooksAPI'
 
 class App extends Component {
-	render() {
-		return (
-			<div>
-				hello from root
+
+state = {
+	books: []
+}
+
+componentDidMount(){
+	BooksAPI.getAll().then((books) => {
+		this.setState({books: books})
+		console.log(books);
+	})
+}
+
+render() {
+	return (
+		<div>
+			hello from root
       </div>
-		)
-	}
+	)
+}
 }
 
 export default App;
